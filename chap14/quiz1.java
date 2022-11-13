fad81cabd69b2da4d614dc8e66ebc398b52e31e3
nowsilver <46918839+rosa2070@users.noreply.github.com>
2022-11-13 22:49:00 +0900

    Update quiz1.java

--- chap14/quiz1.java
+++ chap14/quiz1.java
@@ -2,7 +2,7 @@ class Worker extends Thread {
     @Override
     public void run() {
         for (int i = 0; i < 5; i++) {
-            System.out.println(i);
+            System.out.println("작업 스레드 : " + i);
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
@@ -19,7 +19,7 @@ public class quiz1 {
         t.start();
 
         for (int i = 0; i < 26; i++) {
-            System.out.print((char)alphabet + " " );
+            System.out.println("메인스레드 : " + (char)alphabet);
             alphabet++;
             try {
                 Thread.sleep(500);
