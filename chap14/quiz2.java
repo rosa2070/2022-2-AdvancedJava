45b0b3c44bbed6e9e51fa3b55dabbcdab43784a3
nowsilver <46918839+rosa2070@users.noreply.github.com>
2022-11-13 22:51:45 +0900

    Add files via upload

---
+++ chap14/quiz2.java
@@ -0,0 +1,25 @@
+public class quiz2 {
+    public static void main(String[] args) {
+        int alphabet = 'a';
+
+        new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("작업 스레드 : " + i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                }
+            }
+        }).start();
+        for (int i = 0; i < 26; i++) {
+            System.out.println("메인 스레드 : " + (char) alphabet);
+            alphabet++;
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+}
+
+
